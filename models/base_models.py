7b04b843c9d2e1d6a52df1a304103cc5edb4f9ec
HestiaSky <848318504@qq.com>
2020-01-03 19:58:56 +0800

    multi label fix bug in loss

--- models/base_models.py
+++ models/base_models.py
@@ -106,6 +106,8 @@ class MLModel(BaseModel):
     def compute_metrics(self, embeddings, data, split):
         idx = data[f'idx_{split}']
         output = self.decode(embeddings, data['adj_train_norm'], idx)
+        print(output)
+        print(Variable(torch.FloatTensor(data['labels'][idx])))
         loss = F.binary_cross_entropy_with_logits(output, Variable(torch.FloatTensor(data['labels'][idx])), self.weights)
         acc, f1_micro, f1_macro, auc_micro, auc_macro = acc_f1_auc(output, data['labels'][idx], average=self.f1_average)
         metrics = {'loss': loss, 'acc': acc, 'f1_micro': f1_micro, 'f1_macro': f1_macro, 'auc_micro': auc_micro, 'auc_macro': auc_macro}
