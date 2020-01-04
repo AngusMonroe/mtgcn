a17438f35d1635050432cb8b4f946fe9cc9ee87a
HestiaSky <848318504@qq.com>
2020-01-04 20:05:46 +0800

    debug

--- utils/data_utils.py
+++ utils/data_utils.py
@@ -175,11 +175,19 @@ def load_medical_data(dataset_str, use_feats, data_path, split_seed=None):
     nb_val = round(0.10 * len(all_idx))
     nb_test = round(0.20 * len(all_idx))
     idx_val, idx_test, idx_train = all_idx[:nb_val], all_idx[nb_val: nb_val+nb_test], all_idx[nb_val+nb_test:]
-    for i in range(10):
-        print(graph.keys()[i])
+    i = 0
+    for k in graph.keys():
+        if i > 10:
+            break
+        print(k)
+        i += 1
     adj = nx.adjacency_matrix(nx.from_dict_of_lists(graph))
-    for i in range(10):
-        print(nx.to_dict_of_lists(adj).keys()[i])
+    i = 0
+    for k in nx.to_dict_of_lists(adj).keys():
+        if i > 10:
+            break
+        print(k)
+        i += 1
     if not use_feats:
         features = sp.eye(adj.shape[0])
     return adj, features, labels, idx_train, idx_val, idx_test
